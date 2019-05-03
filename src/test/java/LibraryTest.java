fb9584132f49e8f941c4e4d76a2ee6d5fa4856a4
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-05-03 12:00:21 +0530

    Create LibraryTest.java

---
+++ src/test/java/LibraryTest.java
@@ -0,0 +1,10 @@
+package LittlePassport.APITesting;
+
+import org.testng.annotations.Test;
+
+public class LibraryTest {
+    @Test public void testSomeLibraryMethod() {
+        Library classUnderTest = new Library();
+        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    }
+}
